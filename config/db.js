8eefe944f06c636bfa771731f01d37d7b6e69825
Wojtek <gggolabek@gmail.com>
2020-10-04 19:56:02 +0200

    add GET all profile and GET user profile by user id requests

--- config/db.js
+++ config/db.js
@@ -4,12 +4,12 @@ const db = config.get("mongoURI");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(
-      db,
-      { useNewUrlParser: true },
-      { useUnifiedTopology: true },
-      { useCreateIndex: true }
-    );
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+    });
     console.log("Mongo connected...");
   } catch (err) {
     console.error(err.message);
